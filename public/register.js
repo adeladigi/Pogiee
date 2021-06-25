88c41d2cf4f9d0a8845730639e5833796063c79f
Marcos <greendogpop@gmail.com>
2021-06-25 15:32:46 -0400

    small things

--- public/register.js
+++ public/register.js
@@ -12,37 +12,36 @@ logo.addEventListener("click",function(){
 });
 
 
-
-// form validation
-const form = document.querySelector("form");
-const errorMessage = document.getElementById("error-message");
-const password = document.getElementById("exampleInputPassword1");
-const stripe = document.getElementById("strip-s");
-
-
-form.addEventListener("submit",function(e){
-  let messages = [];
-
-  errorMessage.innerHTML = messages;
-
-  if(password.value.length <= 6){
-    messages.push("Password must be longer then 6 characters");
-  }
-
-
-  if(password.value.length >= 20) {
-    messages.push("Password must not be longer then 20 characters")
-  }
-
-  if(password.value === "password" || password.value === "Password") {
-    messages.push("Password can not be password");
-  }
-
-
-  if(messages.length > 0){
-     e.preventDefault();
-         
-     errorMessage.innerText = messages.join(", ")
-  }
-
-});
+// // form validation
+// const form = document.querySelector("form");
+// const errorMessage = document.getElementById("error-message");
+// const password = document.getElementById("exampleInputPassword1");
+// const stripe = document.getElementById("strip");
+//
+//
+// form.addEventListener("submit",function(e){
+//   let messages = [];
+//
+//   errorMessage.innerHTML = messages;
+//
+//   if(password.value.length <= 6){
+//     messages.push("Password must be longer then 6 characters");
+//   }
+//
+//
+//   if(password.value.length >= 20) {
+//     messages.push("Password must not be longer then 20 characters")
+//   }
+//
+//   if(password.value === "password" || password.value === "Password") {
+//     messages.push("Password can not be password");
+//   }
+//
+//
+//   if(messages.length > 0){
+//     e.preventDefault();
+//
+//     errorMessage.innerText = messages.join(", ")
+//   }
+//
+// });
