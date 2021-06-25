ebeaf273e08d3933681677dd3c8d92d5740c59d5
Marcos <greendogpop@gmail.com>
2021-06-25 12:03:36 -0400

    added log in button to forgot password page

---
+++ public/forgot-password.js
@@ -0,0 +1,13 @@
+
+
+const logoBtn = document.getElementById("logo-btn");
+const loginBtn = document.getElementById("log");
+
+
+logoBtn.addEventListener("click",function(){
+  window.location.href = "/";
+});
+
+loginBtn.addEventListener("click",function(){
+  window.location.href = "/login";
+});
