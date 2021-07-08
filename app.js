4b230262354055def02af5ff9f6bb640e6fca3c7
Marcos <greendogpop@gmail.com>
2021-07-08 15:27:53 -0400

    fixed misspel in email link

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ const secret =  JWT_SECRET;
 
     const token = jwt.sign(payload, secret, {expiresIn: "15m"});
     const link = "http://www.pogiee.com/reset-password/"+newID+"/"+token;
-    const text = "You are receiving this because you (or someone else) have requsted the reset of the password of your pogiee account."+
+    const text = "You are receiving this because you (or someone else) have requested the reset of the password of your pogiee account."+
                  "Please click on the following link, or paste this into your browser to complete the process. If you did not request a password "+
                  "reset then contact Pogiee suppoprt."+"\n\n"+link
 
